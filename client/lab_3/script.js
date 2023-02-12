87a7e321ca4f5b8571c400bef25ad6ef1d6c4154
Alexis Goldberg <alexisaeg12@gmail.com>
2023-02-12 15:55:22 -0500

    added prev button

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -23,6 +23,7 @@ function updateSlidePosition() {
   slide.classList.remove('visible');
   slide.classList.add('hidden');
   });
+  console.log(slidePosition);
   slides[slidePosition].classList.add('visible');
   // Using the .forEach array method, (array.forEach((element) => { per-element work goes here }))
   // loop through all the slides in your slideArray
@@ -40,6 +41,11 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+ if (slidePosition===totalSlides -1){
+  slidePosition=0
+ } else {
+  slidePosition += 1
+ }
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
@@ -50,6 +56,11 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+    if (slidePosition===0){
+      slidePosition=totalSlides-1
+     } else {
+      slidePosition -= 1
+     }
   updateSlidePosition();
 }
 
@@ -65,3 +76,7 @@ document.querySelector('.next') // Get the appropriate element (<button class="n
 
 // Paying close attention to the above queryselector, write one that fires
 // when you want a "prev" slide
+document.querySelector('.prev').addEventListener('click',()=>{
+  console.log('clicked prev');
+  moveToPrevSlide();
+});
